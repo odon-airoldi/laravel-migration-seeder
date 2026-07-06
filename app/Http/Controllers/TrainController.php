d0d3631b9718cc81e76aedf2791aca418b927c45
odon-airoldi <odon.airoldi@gmail.com>
2026-07-06 16:26:14 +0200

    Sort trains by departure time in index and departures views for improved clarity

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -9,16 +9,14 @@ class TrainController extends Controller
 {
     public function indexTrains() {
 
-        $allTrains = Train::all();
+        $allTrains = Train::orderBy('orario_partenza', 'asc')->get();
 
         return view('index', compact('allTrains'));
     }
 
     public function departuresTrains() {
 
-        $trains = Train::all();
-
-        $departingTrains = Train::where('orario_partenza', '>', now())->get();
+        $departingTrains = Train::where('orario_partenza', '>', now())->orderBy('orario_partenza', 'asc')->get();
 
         return view('trains', compact('departingTrains'));
     }
--- resources/views/components/train-timetable.blade.php
+++ resources/views/components/train-timetable.blade.php
@@ -4,6 +4,7 @@
     <li>
         Destinazione: {{ $train->stazione_arrivo }}
         <ul>
+            <li>Data partenza {{ $train->orario_partenza->format('d/m/Y') }}</li>
             <li>Orario Partenza {{ $train->orario_partenza->format('H:i') }}</li>
             <li>Provenienza: {{ $train->stazione_partenza }}</li>
             <li>Codice {{ $train->codice_treno }}</li>
